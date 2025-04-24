97f5e30092b945f88b485de0ed4a812fd0e1cbde
therealdanmalmx <danmalmx@gmail.com>
2025-04-25 00:38:09 +0200

    Minor fixes

--- src/components/footer/FooterCopyright.tsx
+++ src/components/footer/FooterCopyright.tsx
@@ -1,14 +1,6 @@
-import React from 'react'
-
-const copyRightDate = () => {
-    const date = new Date();
-    const year = date.getFullYear();
-    return year;
-}
-
 const FooterCopyright = () => {
   return (
-    <div className='absolute bottom-10 text-2xl text-center text-[var(--body-color)]'>© {copyRightDate()} <span className='text-[var(--primary-color)]'>Pixel Rebellion.</span> All rights reserved.</div>
+    <div className='absolute bottom-10 text-2xl text-center text-[var(--body-color)]'>© {new Date().getFullYear()} <span className='text-[var(--primary-color)]'>Pixel Rebellion.</span> All rights reserved.</div>
   )
 }
 
--- src/components/footer/FooterList.tsx
+++ src/components/footer/FooterList.tsx
@@ -9,7 +9,7 @@ const FooterList = () => {
                 <div key={item.id} className='flex flex-col items-center justify-between'>
                     <div className='flex items-center justify-center'>
                         <p className='text-[1.8rem] text-center text-[var(--body-color)] w-[150px] mt-2'><Link href={item.link}>{item.title}</Link></p>
-                        <p className='invisible md:visible text-white font-bold'>{item.title !== 'Contact' && '\\'}</p>
+                        <p className='invisible md:visible text-white text-2xl font-bold'>{item.title !== 'Contact' && '\\'}</p>
                     </div>
                 </div>
             ))}
