b8fd80c10f399d72cdfafc9efd3a16911b680a25
therealdanmalmx <danmalmx@gmail.com>
2025-04-25 11:38:17 +0200

    Adjust width of WhatWDoCard on mobile

--- src/components/whatWeDo/WhatWeDoCard.tsx
+++ src/components/whatWeDo/WhatWeDoCard.tsx
@@ -14,7 +14,7 @@ const WhatWeDoCard = ({id, title, description, icon}: WhatWeDoCardProps) => {
         <div className='flex flex-col gap-2 items-center justify-center h-full'>
             <div className='mb-12'>{icon}</div>
             <h2 className="text-white text-center text-[3.2rem] font-bold my-2">{title}</h2>
-            <p className="text-gray-400 text-[1.8rem] w-[400px] leading-relaxed">
+            <p className="text-gray-400 text-[1.8rem] w-11/12 md:w-[400px] leading-relaxed">
             {description}
             </p>
         </div>
