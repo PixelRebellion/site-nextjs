48c0d071b6005d3a442a6da646a5ae5274348f30
therealdanmalmx <danmalmx@gmail.com>
2025-04-27 20:27:05 +0200

    Move instantiation isnide function

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,9 +1,9 @@
 import { EmailTemplate } from '../../../components/email-template';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     // Parse the request body
     const body = await request.json();
