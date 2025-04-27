27164f2729a088022c80fb00eee2f25d8d3f8737
therealdanmalmx <danmalmx@gmail.com>
2025-04-27 23:16:53 +0200

    Update sendmailbutton

--- src/components/hero/SendMailButton.tsx
+++ src/components/hero/SendMailButton.tsx
@@ -1,39 +1,44 @@
 "use client";
 
+
 const sendEmail = async () => {
-  try {
-    const response = await fetch("/api/send/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        firstName: "You!",
-      }),
-    });
+ try {
+   const response = await fetch("/api/send", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+     },
+     body: JSON.stringify({
+       firstName: "You!",
+     }),
+   });
 
-    if (!response.ok) {
-      const errorData = await response.json();
-      console.error("Email sending failed:", errorData);
-      throw new Error(`Failed to send email: ${response.status}`);
-    }
 
-    const data = await response.json();
-    console.log("Email sent successfully:", data);
-  } catch (error) {
-    console.error("Error sending email:", error);
-  }
+   if (!response.ok) {
+     const errorData = await response.json();
+     console.error("Email sending failed:", errorData);
+     throw new Error(`Failed to send email: ${response.status}`);
+   }
+
+
+   const data = await response.json();
+   console.log("Email sent successfully:", data);
+ } catch (error) {
+   console.error("Error sending email:", error);
+ }
 }
+
 
 const SendMailButton = () => {
-  return (
-    <button
-      onClick={sendEmail}
-      className="px-6 py-2 border border-black rounded-lg cursor-pointer"
-    >
-      Send mail
-    </button>
-  )
+ return (
+   <button
+     onClick={sendEmail}
+     className="px-6 py-2 border border-black rounded-lg cursor-pointer"
+   >
+     Send mail
+   </button>
+ )
 }
+
 
 export default SendMailButton;
