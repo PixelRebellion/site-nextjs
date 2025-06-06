42c743f1290884ad8e9a72ad1960837e0209b2c2
therealdanmalmx <danmalmx@gmail.com>
2025-06-06 16:19:59 +0200

    Add h1 tag

--- src/app/rebel-work/RebelWorkHeading.tsx
+++ src/app/rebel-work/RebelWorkHeading.tsx
@@ -4,7 +4,7 @@ const RebelWorkHeading = () => {
   return (
     <div className="bg-cover bg-center w-full bg-no-repeat bg-fixed bg-[url(/assets/backgrounds/services.svg)]">
         <div className='mt-48 mb-24 md:mt-36 md:mb-36'>
-            <h3 className='text-[4.8rem] text-center mx-auto text-[var(--heading-color)]'>Our work</h3>
+            <h1 className='text-[4.8rem] text-center mx-auto text-[var(--heading-color)]'>Our work</h1>
         </div>
     </div>
   )
