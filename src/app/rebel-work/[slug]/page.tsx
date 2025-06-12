1e8739838d2e071e294b717955827e45252a499a
therealdanmalmx <danmalmx@gmail.com>
2025-06-12 10:02:33 +0200

    Chnage layout of images on cases

--- src/app/rebel-work/[slug]/page.tsx
+++ src/app/rebel-work/[slug]/page.tsx
@@ -31,7 +31,7 @@ const RebelWorksPageShowcase = async ({ params }: { params: Promise<PageParams> 
     <div className='w-full h-full bg-[var(--background-color)]'>
       <div className='flex flex-col items-center justify-center'>
         <Image src={`/assets/images/${workItem!.icon[0]}.webp`} width={1000} height={1000} alt={`${workItem!.title} image`} className='mt-48 w-10/12 rounded-xl mx-auto md:mx-0' />
-        <h1 className='w-10/12 text-center md:text-left text-[3.2rem] text-[var(--heading-color)] my-12'>{workItem!.title}</h1>
+        <h1 className='w-full md:w-10/12 text-center md:text-left text-[3.2rem] text-[var(--heading-color)] my-12'>{workItem!.title}</h1>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 w-10/12 mx-auto space-y-24'>
         <div className='w-full md:w-10/12'>
@@ -51,9 +51,9 @@ const RebelWorksPageShowcase = async ({ params }: { params: Promise<PageParams> 
           <p className='text-[1.8rem] text-[var(--body-color)]'>{workItem!.outcome}</p>
         </div>
       </div>
-      <div className='flex flex-wrap w-11/12 justify-center items-center mx-auto my-36 space-y-10'>
+      <div className='flex flex-col w-full justify-center items-center my-36 space-y-10'>
       {workItem!.icon!.map((icon) => (
-        <Image src={`/assets/images/${icon}.webp`} width={1000} height={1000} alt={`${workItem!.title} image`} className='w-10/12 rounded-xl mx-auto md:mx-0'/>
+        <Image src={`/assets/images/${icon}.webp`} width={1000} height={1000} alt={`${workItem!.title} image`} className='w-10/12 rounded-xl'/>
       ))}
       </div>
       <CallToAction
