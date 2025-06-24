d6a2a933fc4ff5080a2b3b207bf3e579175dfece
therealdanmalmx <danmalmx@gmail.com>
2025-06-24 18:53:09 +0100

    Fix sequencial order of heading

--- src/components/common/hero/HeroIntroText.tsx
+++ src/components/common/hero/HeroIntroText.tsx
@@ -8,7 +8,7 @@ interface IntroTextProps {
 const HeroIntroText = ({title, description}: IntroTextProps) => {
   return (
     <div className="h-full w-full space-y-4 my-36">
-    <h3 className="text-[3.8rem] xs:text-[4.5rem] md:text-[6.4rem] text-center text-white font-bold leading-tight">{title}</h3>
+    <h1 className="text-[3.8rem] xs:text-[4.5rem] md:text-[6.4rem] text-center text-white font-bold leading-tight">{title}</h1>
     <p className="mx-auto w-11/12 md:max-w-[60rem] text-center text-[var(--body-color)] text-[1.8rem] leading-[150%]">{description}</p>
   </div>
   )
