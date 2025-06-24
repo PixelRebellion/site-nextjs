9a8e36d1037d829309a13bd3a841eb27d6551edf
therealdanmalmx <danmalmx@gmail.com>
2025-06-24 20:06:22 +0100

    Add JS minification to config

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,12 @@ const nextConfig: NextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
+    webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.optimization.minimize = true
+    }
+    return config
+  }
 };
 
 export default nextConfig;
