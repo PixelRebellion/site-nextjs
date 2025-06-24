5bd33a7b44dd62a559b1914ba95ab40046459db9
therealdanmalmx <danmalmx@gmail.com>
2025-06-24 19:11:46 +0100

    Add tree shaking

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+   experimental: {
+    optimizePackageImports: ['react-hook-form', 'react-hot-toast', 'resend', 'sitemap', 'uuid', 'tailwindcss']
+  }
   // output:  'export',
 
   // images: {
