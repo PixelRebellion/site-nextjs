2f771b3110b003b92237f4ad8fc7f493e43c13ee
therealdanmalmx <danmalmx@gmail.com>
2025-06-24 18:56:55 +0100

    Try to fix sequencial heading

--- src/components/rebelWork/RebelWorkList.tsx
+++ src/components/rebelWork/RebelWorkList.tsx
@@ -11,7 +11,7 @@ const RebelWorkList = () => {
         <div key={work.id} className='flex flex-col items-start justify-center space-y-12 md:space-y-16'>
           <div>
             <Image src={`/assets/images/${work.icon[0]}.webp`} width={1000} height={1000} alt={`${work.title} image`} className='w-[450px] h-[300px] md:max-w-[50rem] mx-auto md:mx-0 object-fill' />
-            <h3 className='text-[2.4rem] text-white text-center md:text-left font-bold mt-4'>{work.title}</h3>
+            <h1 className='text-[2.4rem] text-white text-center md:text-left font-bold mt-4'>{work.title}</h1>
             <p className='text-[1.8rem] text-center md:text-left text-[var(--body-color)] w-11/12 md:w-[400px] mx-auto md:mx-0 mt-2'>{`${work.description.split('.')[0]}.`}</p>
           </div>
           <Link href={`/rebel-work/${createSlug(work.title)}`}><button className='w-[18rem] h-[5.5rem] border text-xl rounded-lg text-[var(--heading-color)] border-white cursor-pointer'>Full case study </button></Link>
